9994d6b696ac9b027fbf59f6588cb7feedb2d249
Manish Patil <94184147+Manya009@users.noreply.github.com>
2025-04-08 17:36:30 +0530

    added checks for transactions both

--- backend/code_for_extraction.py
+++ backend/code_for_extraction.py
@@ -675,7 +675,12 @@ def cleaning(new_df):
 
     df = check_date(df)
     df = df[df['Balance'].notna() & (df['Balance'] != "")]
-    df = df[~((df["Debit"].fillna(0) == 0) & (df["Credit"].fillna(0) == 0))]
+    
+    df = df[~(
+    ((df["Debit"].fillna(0) == 0) & (df["Credit"].fillna(0) == 0)) |
+    ((df["Debit"].fillna(0) > 0) & (df["Credit"].fillna(0) > 0))
+    )]
+    
     df = df[["Value Date", "Description", "Debit", "Credit", "Balance"]]
     df = df.drop_duplicates()
     idf = df.reset_index(drop=True)
