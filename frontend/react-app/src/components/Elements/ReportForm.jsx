d618e2467828275da8ce7a9721aa6149324a6ef4
Aiyaz17 <68592281+Aiyaz17@users.noreply.github.com>
2025-05-05 21:34:02 +0530

    chngs

--- frontend/react-app/src/components/Elements/ReportForm.jsx
+++ frontend/react-app/src/components/Elements/ReportForm.jsx
@@ -824,7 +824,7 @@ const GenerateReportForm = ({
                             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
                               <div>
                                 <label className="block text-xs font-medium text-gray-700 dark:text-gray-300 mb-1">
-                                  Bank Name
+                                  Bank Name *
                                 </label>
                                 <Select
                                   value={detail.bankName || ""}
