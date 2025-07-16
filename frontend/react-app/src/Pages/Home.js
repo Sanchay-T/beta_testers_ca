83ab1127f95f6e0b5efe57761c1d0e2eb92605a4
Aiyaz Qureshi <qureshi.aiyaz123@gmail.com>
2025-07-16 16:46:29 +0530

    changes

--- frontend/react-app/src/Pages/Home.js
+++ frontend/react-app/src/Pages/Home.js
@@ -58,11 +58,11 @@ const Dashboard = () => {
       url: "#",
       icon: Grid2X2,
     },
-     {
-      title: "Billing",
-      url: "#",
-      icon: Grid2X2,
-    },
+    //  {
+    //   title: "Billing",
+    //   url: "#",
+    //   icon: Grid2X2,
+    // },
     // {
     //   title: "Import to Tally",
     //   url: "#",
--- frontend/react-app/src/components/MainDashboardComponents/MainDashboard.js
+++ frontend/react-app/src/components/MainDashboardComponents/MainDashboard.js
@@ -783,7 +783,7 @@ const MainDashboard = ({ handleTabChange }) => {
               </h2>{" "}
               <span className="bg-gradient-to-r from-blue-500 to-cyan-400 text-white text-xs px-2 py-1 rounded-full font-semibold animate-pulse">
                 {" "}
-                v2.1.000{" "}
+                v2.1.200{" "}
               </span>{" "}
             </div>{" "}
             <p className="text-muted-foreground">
